cd2b469783ac8fdfe24702ff129b217601f13e40
RZanfelice <rafael.zanf@gmail.com>
2024-09-20 11:41:20 -0300

    Refactor user-related code and remove unused files

--- src/api/medications/medication_users.py
+++ src/api/medications/medication_users.py
@@ -1,55 +1,58 @@
 from datetime import date, datetime
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from sqlmodel import Field, SQLModel, Session, select
-from schema.medications.medication_users import MedicationUser
+from src.schema.schema import UserBase
+from src.schema.schema import UserCreate
+from src.schema.schema import UserUpdate
+from src.schema.schema import UserPublic
+from src.schema.schema import Users
 from db.manager import Database
 
+BASE_URL_USERS = "/medication/user"
 medications_user_router = APIRouter()
 
-@ medications_user_router.post("/medication/create_user")
-def create_user(user: MedicationUser):
+@medications_user_router.post(BASE_URL_USERS)
+def create_user(
+    *,
+    session: Session = Depends(Database.get_session),
+    user: UserCreate
+):
     """Create a new user"""
-    with Session(Database.db_engine()) as session:
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return user
+    dates = {"created_at": datetime.now(), "updated_at": datetime.now()}
+    db_user = Users.model_validate(user, update=dates)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return user
 
-@ medications_user_router.get("/medication/read_all_users")
-def read_all_users():
-    """Read all users"""
-    with Session(Database.db_engine()) as session:
-        users = session.exec(select(MedicationUser)).all()
-        return users
+@medications_user_router.get(BASE_URL_USERS + "{user_id}", response_model=UserPublic)
+def get_user_by_id(
+        *,
+        session: Session = Depends(Database.get_session),
+        user_id: int
+):
+    """Get specific patient"""
+    user = session.get(Users, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
-@ medications_user_router.get("/medication/read_one_user")
-def read_one_user(user_id):
-    """Read one user"""
-    with Session(Database.db_engine()) as session:
-        user = session.exec(select(MedicationUser).where(MedicationUser.id == user_id))
-        return user
-    
-@ medications_user_router.put("/medication/update_user")
-def update_user(user_id, user: MedicationUser):
-    """Update a user"""
-    with Session(Database.db_engine()) as session:
-        user = session.exec(select(MedicationUser).where(MedicationUser.id == user_id))
-        user.name = user.name
-        user.email = user.email
-        user.birthday = user.birthday
-        user.phone_number = user.phone_number
-        user.emergency_contact_name = user.emergency_contact_name
-        user.emergency_contact_number = user.emergency_contact_number
-        user.accept_tcle = user.accept_tcle
-        session.commit()
-        session.refresh(user)
-        return user
-    
-@ medications_user_router.delete("/medication/delete_user")
-def delete_user(user_id):
-    """Delete a user"""
-    with Session(Database.db_engine()) as session:
-        user = session.exec(select(MedicationUser).where(MedicationUser.id == user_id))
-        session.delete(user)
-        session.commit()
-        return user
+
+@medications_user_router.patch(BASE_URL_USERS + "{user_id}", response_model=UserPublic)
+def update_user(
+        *,
+        session: Session = Depends(Database.get_session),
+        patient_id: int,
+        patient: UserUpdate
+):
+    """Update User"""
+    db_user = session.get(Users, patient_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    patient_data = patient.model_dump(exclude_unset=True)
+    updated_at = {"updated_at": datetime.now()}
+    db_user.sqlmodel_update(patient_data, update=updated_at)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
