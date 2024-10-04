ac21898282dc596b1583416fd12a89d6387987f4
RZanfelice <rafael.zanf@gmail.com>
2024-10-04 13:13:52 -0300

    add relationships table and some test data

--- src/api/schemas/models.py
+++ src/api/schemas/models.py
@@ -1,137 +1,167 @@
-from datetime import  datetime
+from datetime import date, datetime
 from sqlmodel import Field, SQLModel, Relationship
-from sqlmodel import SQLModel, Field, Relationship
-from typing import List, Optional
-from datetime import datetime
-
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str
-    email: str
-    birth_date: datetime
-    created_at: datetime
-    updated_at: datetime
-
-    # Relationships to other tables
-    diseases: List["UserDisease"] = Relationship(back_populates="user")
-    drugs: List["UserDrug"] = Relationship(back_populates="user")
-    tracking_records: List["TrackingRecord"] = Relationship(back_populates="user")
-
-class UserCreate(SQLModel):
-    name: str
-    email: str
-    birth_date: datetime
-
-class UserPublic(SQLModel):
-    name: str
-    email: str
-    birth_date: datetime
-
-class UserUpdate(SQLModel):
-    name: str
-    email: str
-    birth_date: datetime
-
-class Drug(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    code: int
-    reference_drug: str
-    commercial_name: str
-    farmaco: str
-    prescription: str
-    format: str
-    dosage_strength: str
-    route: str
-    adult_dosage: str
-    pediatric_dosage: str
-    contradictions: str
-    drug_leaflet_path: str
-    is_sus_available: bool
-    average_price: float
-    expected_effects: str
-    adverse_effects: str
-    indication_for_use: str
-    how_to_take: str
-    is_splitable: bool
-    is_macerable: bool
-    precautions: str
-
-    # Relationship fields
-    food_interactions: List["FoodInteraction"] = Relationship(back_populates="drug")
-    drug_interactions: List["DrugInteraction"] = Relationship(back_populates="drug_a")
 
 
-class DrugInteraction(SQLModel, table=True):
-    drug_a_id: int = Field(foreign_key="drug.id", primary_key=True)
-    drug_b_id: int = Field(foreign_key="drug.id", primary_key=True)
-    interaction: str
-
-    # Relationships
-    drug_a: Drug = Relationship(back_populates="drug_interactions")
-    drug_b: Drug = Relationship()
-
-
-class Food(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    food_name: str
-
-    # Backref for interactions
-    drugs: List["FoodInteraction"] = Relationship(back_populates="food")
-
-
-class FoodInteraction(SQLModel, table=True):
-    drug_id: int = Field(foreign_key="drug.id", primary_key=True)
-    food_id: int = Field(foreign_key="food.id", primary_key=True)
-    interaction: str
-
-    # Relationships
-    drug: Drug = Relationship(back_populates="food_interactions")
-    food: Food = Relationship(back_populates="drugs")
-
-
-class Disease(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    disease_name: str
-    description: Optional[str] = None
-
-
-class Caretaker(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    created_at: datetime
-    updated_at: datetime
-
-
+""" RELATIONSHIPS TABLES """
 class UserDisease(SQLModel, table=True):
     user_id: int = Field(foreign_key="user.id", primary_key=True)
     disease_id: int = Field(foreign_key="disease.id", primary_key=True)
     created_at: datetime
     updated_at: datetime
-    status: Optional[str] = None
+    status: str | None
 
-    # Relationship to the User and Disease
-    user: User = Relationship(back_populates="diseases")
-    disease: "Disease" = Relationship()
+    user: "User" = Relationship(back_populates="disease_links")
+    disease: "Disease" = Relationship(back_populates="user_links")
 
+class UserCaretaker(SQLModel, table=True):
+    user_id: int = Field(foreign_key="user.id", primary_key=True)
+    caretaker_id: int = Field(foreign_key="caretaker.id", primary_key=True)
 
-class UserDrug(SQLModel, table=True):
+    user: "User" =  Relationship(back_populates="caretaker_links")
+    caretaker: "Caretaker" = Relationship(back_populates="user_links")
+
+# class TrackingRecords(SQLModel, table=True):
+#     user_id: int = Field(foreign_key="user.id", primary_key=True)
+#     code: int = Field(foreign_key="drug.id", primary_key=True)
+#     created_date: datetime
+#     took_date: datetime
+#     is_taken: bool
+
+class UserUseDrug(SQLModel, table=True):
     user_id: int = Field(foreign_key="user.id", primary_key=True)
     drug_id: int = Field(foreign_key="drug.id", primary_key=True)
-    created_date: datetime
-    start_date: datetime
-    end_date: datetime
-    frequency: Optional[str] = None
 
-    # Relationship to the User and Drug
-    user: User = Relationship(back_populates="drugs")
-    drug: "Drug" = Relationship()
+    drug: "Drug" = Relationship(back_populates="user_links")
+    user: "User" = Relationship(back_populates="drug_links")
 
 
-class TrackingRecord(SQLModel, table=True):
-    user_id: int = Field(foreign_key="user.id", primary_key=True)
-    code: int = Field(primary_key=True)
-    created_date: datetime
-    took_date: datetime
-    is_taken: bool
 
-    # Relationship to the User
-    user: User = Relationship(back_populates="tracking_records")
+""" DRUG TABLES """
+class DrugActiveIngredient(SQLModel, table=True):
+    drug_id: int = Field(foreign_key="drug.id", primary_key=True)
+    active_ingredient_id: int = Field(foreign_key="activeingredient.id", primary_key=True)
+
+    drug: "Drug" = Relationship(back_populates="active_ingredient_links")
+    active_ingredient: "ActiveIngredient" = Relationship(back_populates="drug_links")
+
+class DrugPresentations(SQLModel, table=True):
+    drug_id: int = Field(foreign_key="drug.id", primary_key=True)
+    presentation_id: int = Field(foreign_key="presentations.id", primary_key=True)
+
+    drug: "Drug" = Relationship(back_populates="presentations_links")
+    presentations: "Presentations" = Relationship(back_populates="drug_links")
+
+class DrugComercialNames(SQLModel, table=True):
+    drug_id: int = Field(foreign_key="drug.id", primary_key=True)
+    comercial_name_id: int = Field(foreign_key="comercialnames.id", primary_key=True)
+
+    drug: "Drug" = Relationship(back_populates="comercial_names_links")
+    comercial_names: "ComercialNames" = Relationship(back_populates="drug_links")	
+
+class ActiveIngredient(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+
+    drug_links: list[DrugActiveIngredient] = Relationship(back_populates="active_ingredient")
+
+class Presentations(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    value: str
+
+    drug_links: list[DrugPresentations] = Relationship(back_populates="presentations")
+
+class ComercialNames(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    comercial_name: str
+
+    drug_links: list[DrugComercialNames] = Relationship(back_populates="comercial_names")
+
+class Drug(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    code: str
+
+    user_links: list[UserUseDrug] = Relationship(back_populates="drug")
+    active_ingredient_links: list[DrugActiveIngredient] = Relationship(back_populates="drug")
+    presentations_links: list[DrugPresentations] = Relationship(back_populates="drug")
+    comercial_names_links: list[DrugComercialNames] = Relationship(back_populates="drug")
+
+
+""" USER TABLES """
+class UserBase(SQLModel):
+    name: str
+    birthday: date | None = None
+    phone_number: str | None = None
+    emergency_contact_name: str | None = None
+    emergency_contact_number: str | None = None
+    accept_tcle: bool
+
+class UserCreate(UserBase):
+    pass
+
+class UserUpdate(SQLModel):
+    name: str | None = None
+    birthday: datetime | None = None
+    phone_number: str | None = None
+    emergency_contact_name: str | None = None
+    emergency_contact_number: str | None = None
+    accept_tcle: bool | None = None
+
+class UserPublic(UserBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    birthday: date | None = None
+    phone_number: str | None = None
+    emergency_contact_name: str | None = None
+    emergency_contact_number: str | None = None
+    accept_tcle: bool
+    created_at: datetime
+    updated_at: datetime
+
+    drug_links: list[UserUseDrug] = Relationship(back_populates="user")
+    caretaker_links: list[UserCaretaker] = Relationship(back_populates="user")
+    disease_links: list[UserDisease] = Relationship(back_populates="user")
+
+class Disease(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    description: str | None = None
+
+    user_links: list[UserDisease] = Relationship(back_populates="disease")
+
+class UserWithDrugsRead(UserBase):
+    drugs: list[Drug]
+
+
+""" CARETAKER TABLES """
+class CaretakerBase(SQLModel):
+    email: str
+    name: str
+    phone_number: str | None = None
+
+class CaretakerCreate(CaretakerBase):
+    pass
+
+class CaretakerPublic(CaretakerBase):
+    caretaker_id: int
+    created_at: datetime
+    updated_at: datetime
+
+class CaretakerUpdate(SQLModel):
+    email: str | None = None
+    name: str | None = None
+    phone_number: str | None = None
+
+class Caretaker(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    email: str
+    name: str
+    phone_number: str | None = None
+    created_at: datetime
+    updated_at: datetime
+
+    user_links: list[UserCaretaker] = Relationship(back_populates="caretaker")
--- src/db/manager.py
+++ src/db/manager.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine, Session
 
 import db.config as config
-from api.schemas.models import User, Drug, UserDrug, DrugInteraction, Food, FoodInteraction, Disease, UserDisease, Caretaker, TrackingRecord
+from api.schemas.models import *
 
 class Database:
     """Database class to handle database connection and operations"""
@@ -13,15 +13,145 @@ class Database:
         """Create the database and tables that do not exist"""
         User.metadata.create_all(self.engine)
         Drug.metadata.create_all(self.engine)
-        UserDrug.metadata.create_all(self.engine)
-        DrugInteraction.metadata.create_all(self.engine)
-        Food.metadata.create_all(self.engine)
-        FoodInteraction.metadata.create_all(self.engine)
         Disease.metadata.create_all(self.engine)
         UserDisease.metadata.create_all(self.engine)
         Caretaker.metadata.create_all(self.engine)
-        TrackingRecord.metadata.create_all(self.engine)
-        
+        #TrackingRecords.metadata.create_all(self.engine)
+    
+    def add_data(self):
+        """Add data to the database"""
+        with Session(self.engine) as session:
+            """ ADD USERS """
+            user1 = User(name="John Doe", 
+                        email="email@email.com", 
+                        birth_date="1990-01-01", 
+                        phone_number="123456789", 
+                        emergency_contact_name="Jane Doe", 
+                        emergency_contact_number="987654321", 
+                        accept_tcle=True,
+                        created_at="2021-01-01",
+                        updated_at="2021-01-01")
+            session.add(user1)
+
+            user2 = User(name="Jane Doe", 
+                        email="email2@email.com",  
+                        birth_date="1990-01-01",
+                        phone_number="123456789",
+                        emergency_contact_name="John Doe",
+                        emergency_contact_number="987654321",
+                        accept_tcle=True,
+                        created_at="2021-01-01",
+                        updated_at="2021-01-01")
+            session.add(user2)
+
+            session.commit()
+            session.refresh(user1)
+            session.refresh(user2)
+ 
+            """ ADD DRUGS """
+            levotiroxina = Drug(code = "51-48-9")
+            levotiroxina_active_ingredient = ActiveIngredient(name="levotiroxina")
+            levotiroxina_active_ingredient_link = DrugActiveIngredient(drug=levotiroxina, active_ingredient=levotiroxina_active_ingredient)
+            levotiroxina_presentation_25 = Presentations(name="25uG")
+            levotiroxina_presentation_50 = Presentations(name="50uG")
+            levotiroxina_presentation_75 = Presentations(name="75uG")
+            levotiroxina_presentation_100 = Presentations(name="100uG")
+            levotiroxina_presentation_link_25 = DrugPresentations(drug=levotiroxina, presentations=levotiroxina_presentation_25)
+            levotiroxina_presentation_link_50 = DrugPresentations(drug=levotiroxina, presentations=levotiroxina_presentation_50)
+            levotiroxina_presentation_link_75 = DrugPresentations(drug=levotiroxina, presentations=levotiroxina_presentation_75)
+            levotiroxina_presentation_link_100 = DrugPresentations(drug=levotiroxina, presentations=levotiroxina_presentation_100)
+            levotiroxina_comercial_name_1 = ComercialNames(name="Synthroid")
+            levotiroxina_comercial_name_2 = ComercialNames(name="Euthyrox")
+            levotiroxina_comercial_name_3 = ComercialNames(name="Puran T4")
+            levotiroxina_comercial_name_link_1 = DrugComercialNames(drug=levotiroxina, comercial_names=levotiroxina_comercial_name_1)
+            levotiroxina_comercial_name_link_2 = DrugComercialNames(drug=levotiroxina, comercial_names=levotiroxina_comercial_name_2)
+            levotiroxina_comercial_name_link_3 = DrugComercialNames(drug=levotiroxina, comercial_names=levotiroxina_comercial_name_3)
+            session.add(levotiroxina)
+            session.add(levotiroxina_active_ingredient)
+            session.add(levotiroxina_active_ingredient_link)
+            session.add(levotiroxina_presentation_25)
+            session.add(levotiroxina_presentation_50)
+            session.add(levotiroxina_presentation_75)
+            session.add(levotiroxina_presentation_100)
+            session.add(levotiroxina_presentation_link_25)
+            session.add(levotiroxina_presentation_link_50)
+            session.add(levotiroxina_presentation_link_75)
+            session.add(levotiroxina_presentation_link_100)
+            session.add(levotiroxina_comercial_name_1)
+            session.add(levotiroxina_comercial_name_2)
+            session.add(levotiroxina_comercial_name_3)
+            session.add(levotiroxina_comercial_name_link_1)
+            session.add(levotiroxina_comercial_name_link_2)
+            session.add(levotiroxina_comercial_name_link_3)
+
+            omeprazol = Drug(code= "73590-58-6")
+            omeprazol_active_ingredient = ActiveIngredient(name="omeprazol")
+            omeprazol_active_ingredient_link = DrugActiveIngredient(drug=omeprazol, active_ingredient=omeprazol_active_ingredient)
+            omeprazol_presentation_20 = Presentations(name="20mg")
+            omeprazol_presentation_link_20 = DrugPresentations(drug=omeprazol, presentations=omeprazol_presentation_20)
+            omeprazol_comercial_name_1 = ComercialNames(name="Losec")
+            omeprazol_comercial_name_2 = ComercialNames(name="Omeprazol EMS")
+            omeprazol_comercial_name_link_1 = DrugComercialNames(drug=omeprazol, comercial_names=omeprazol_comercial_name_1)
+            omeprazol_comercial_name_link_2 = DrugComercialNames(drug=omeprazol, comercial_names=omeprazol_comercial_name_2)
+            session.add(omeprazol)
+            session.add(omeprazol_active_ingredient)
+            session.add(omeprazol_active_ingredient_link)
+            session.add(omeprazol_presentation_20)
+            session.add(omeprazol_presentation_link_20)
+            session.add(omeprazol_comercial_name_1)
+            session.add(omeprazol_comercial_name_2)
+            session.add(omeprazol_comercial_name_link_1)
+            session.add(omeprazol_comercial_name_link_2)
+
+            sinvastatina = Drug(code = "79902-63-9")
+            sinvastatina_active_ingredient = ActiveIngredient(name="sinvastatina")
+            sinvastatina_active_ingredient_link = DrugActiveIngredient(drug=sinvastatina, active_ingredient=sinvastatina_active_ingredient)
+            sinvastatina_presentation_10 = Presentations(name="10mg")
+            sinvastatina_presentation_20 = Presentations(name="20mg")
+            sinvastatina_presentation_40 = Presentations(name="40mg")
+            sinvastatina_presentation_link_10 = DrugPresentations(drug=sinvastatina, presentations=sinvastatina_presentation_10)
+            sinvastatina_presentation_link_20 = DrugPresentations(drug=sinvastatina, presentations=sinvastatina_presentation_20)
+            sinvastatina_presentation_link_40 = DrugPresentations(drug=sinvastatina, presentations=sinvastatina_presentation_40)
+            sinvastatina_comercial_name_1 = ComercialNames(name="Zocor")
+            sinvastatina_comercial_name_2 = ComercialNames(name="Sinvasterol")
+            sinvastatina_comercial_name_3 = ComercialNames(name="Sinvix")
+            sinvastatina_comercial_name_link_1 = DrugComercialNames(drug=sinvastatina, comercial_names=sinvastatina_comercial_name_1)
+            sinvastatina_comercial_name_link_2 = DrugComercialNames(drug=sinvastatina, comercial_names=sinvastatina_comercial_name_2)
+            sinvastatina_comercial_name_link_3 = DrugComercialNames(drug=sinvastatina, comercial_names=sinvastatina_comercial_name_3)
+            session.add(sinvastatina)
+            session.add(sinvastatina_active_ingredient)
+            session.add(sinvastatina_active_ingredient_link)
+            session.add(sinvastatina_presentation_10)
+            session.add(sinvastatina_presentation_20)
+            session.add(sinvastatina_presentation_40)
+            session.add(sinvastatina_presentation_link_10)
+            session.add(sinvastatina_presentation_link_20)
+            session.add(sinvastatina_presentation_link_40)
+            session.add(sinvastatina_comercial_name_1)
+            session.add(sinvastatina_comercial_name_2)
+            session.add(sinvastatina_comercial_name_3)
+            session.add(sinvastatina_comercial_name_link_1)
+            session.add(sinvastatina_comercial_name_link_2)
+            session.add(sinvastatina_comercial_name_link_3)
+
+            session.commit()
+            session.refresh(levotiroxina)
+            session.refresh(omeprazol)
+            session.refresh(sinvastatina)
+
+            """ user1 uses levotiroxina """
+            user1_levotiroxina = UserUseDrug(user_id=user1.id, drug_id=levotiroxina.id)
+            session.add(user1_levotiroxina)
+
+            """ user1 uses omeprazol """
+            user1_omeprazol = UserUseDrug(user_id=user1.id, drug_id=omeprazol.id)
+            session.add(user1_omeprazol)
+
+            """ user2 uses sinvastatina """
+            user2_sinvastatina = UserUseDrug(user_id=user2.id, drug_id=sinvastatina.id)
+            session.add(user2_sinvastatina)
+
+            session.commit()
 
     # Singleton Database instance attribute
     _db_instance = None
@@ -32,6 +162,7 @@ class Database:
 
         if Database._db_instance is None:
             Database._db_instance = Database()
-            Database._db_instance.create_db()
+            # Database._db_instance.create_db()
+            # Database._db_instance.add_data()
 
         return Database._db_instance.engine
