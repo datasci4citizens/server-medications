bdb3779e7b00cc1165831352f78c432d2fc08204
Susane Amabila <susiamabila@gmail.com>
2024-11-14 19:59:03 -0300

    feat(script): script for inserting drugs into the database added

---
+++ src/data/seed_active_principles.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import pandas as pd
+from sqlmodel import Session, select
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+from db.manager import Database
+from api.schemas.models import ActivePrinciple
+
+def add_active_principles():
+    # Lê o Excel com os dados
+    file_path = 'lista_principios_ativos_cas.xlsx'
+    df = pd.read_excel(file_path)
+
+    with Session(Database.db_engine()) as session:
+        for _, row in df.iterrows():
+            # Cria instância do ActivePrinciple com os valores do Excel
+            active_principle = ActivePrinciple(
+                code=row["Nº CAS"],  # Coluna do número CAS
+                active_ingredient=row["DENOMINAÇÃO COMUM BRASILEIRA"].rstrip().capitalize()  # Coluna do nome
+            )
+            session.add(active_principle)
+
+        session.commit()
+        print("Dados inseridos com sucesso.")
+
+if __name__ == "__main__":
+    add_active_principles()
