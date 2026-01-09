cedf6769be64d9836a0d8aced404ca4ac72e6b20
Yanneti2 <m258602@dac.unicamp.br>
2026-01-09 02:06:58 +0000

    path rework + medications

--- lembramed/medication/admin.py
+++ lembramed/medication/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Medication
+admin.site.register(Medication)
--- lembramed/omop/apps.py
+++ lembramed/omop/apps.py
@@ -1,7 +1,5 @@
 from django.apps import AppConfig
 
-teste
-
 class OmopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "omop"
