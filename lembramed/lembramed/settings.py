7283a6fdaeffe95974815ca24dff31b7546ec7db
Yanneti2 <m258602@dac.unicamp.br>
2026-01-19 18:42:39 -0300

    +person and +app_person app

--- lembramed/lembramed/settings.py
+++ lembramed/lembramed/settings.py
@@ -56,7 +56,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders'
+    'corsheaders',
+    'login',
+    'user'
 ]
 
 
---
+++ lembramed/login/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from .models import App_Person
+admin.site.register(App_Person)
