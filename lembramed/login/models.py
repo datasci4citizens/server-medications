7230f89d33a50c014479d10af26afa8f99d71c9b
Yanneti2 <m258602@dac.unicamp.br>
2026-01-20 17:50:01 -0300

    saving hashed password

--- lembramed/login/models.py
+++ lembramed/login/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 class App_Person(models.Model):
     person_id = models.OneToOneField( # mesmo id do person_id do django app PERSON
@@ -8,5 +9,9 @@ class App_Person(models.Model):
     )
     email = models.CharField(max_length=255, blank=True, null=True)
     password = models.CharField(max_length=255, blank=True, null=True)
+    def save(self):
+        if not self.password.startswith('pbkdf2_sha256$'):
+            self.password = make_password(self.password)
+        super().save()
     def __str__(self):
         return self.email
