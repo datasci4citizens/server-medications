7195ed959fa308342eec70a086f600d5a6c01c85
Matheus <m258602@dac.unicamp.br>
2026-01-23 00:20:06 +0000

    final changes to login logic

--- lembramed/accounts/models.py
+++ lembramed/accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 #class App_Person(models.Model):
   #  person_id = models.OneToOneField( # mesmo id do person_id do django app PERSON
@@ -12,13 +13,25 @@ from django.db import models
   #      return self.email
 
 class New_Person(models.Model):
+    # user = models.OneToOneField(
+    #     User,
+    #     null = False,
+    #     blank = False,
+    #     on_delete=models.CASCADE
+    # )
+    person_id = models.UUIDField(
+        default = uuid.uuid4,
+        editable = False,
+        primary_key=True,
+        #unique=True
+    )
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth = models.DateField(blank=True, null=True)
-    email = models.CharField(max_length=255, default="seu_email_aqui@gmail.com")
-    password = models.CharField(max_length=255, default="sua_senha_aqui")
+    email = models.CharField(max_length=255, default="seu_email_aqui@gmail.com") # data from app_person
+    password = models.CharField(max_length=255, default="sua_senha_aqui") # data from app_person
     #gender 
     #race
     #location
     def __str__(self):
-        return self.name
+        return self.name #self.str(person_id)
