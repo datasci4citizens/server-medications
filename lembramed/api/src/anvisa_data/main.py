081648b317ea0e52ffaa144b0622c58b2ea0b5fa
Matheus <m258602@dac.unicamp.br>
2026-02-10 16:19:57 +0000

    Selenium logic fix & register nums treated

---
+++ lembramed/api/src/anvisa_data/main.py
@@ -0,0 +1,20 @@
+from web_navigator import get_bula
+from excel_treatment import get_dict
+
+# abrir o json "anvisa_data.json" em 'data' e pegar o 
+# [4] de cada lista === registerNum
+
+anvisa_dados = get_dict()
+for key,value in anvisa_dados.items():
+    if (key == 'data'):
+        registerNums = [f'{int(dados[4])}' for dados in 
+        value if dados[4]]
+
+# for num in registerNums:
+#     print(num)
+
+# print(f'Penultimo elemento: {registerNums[-2]},\nUltimo elemento: {registerNums[-1]}')
+
+# Pega a bula do paciente de todos os medicamentos registrados no excel da anvisa
+for num in registerNums:
+    get_bula(num)
--- lembramed/api/src/anvisa_data/web_navigator.py
+++ lembramed/api/src/anvisa_data/web_navigator.py
@@ -2,21 +2,38 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
-driver = webdriver.Firefox()
-#driver.implicitly_wait(10)
-driver.get('https://consultas.anvisa.gov.br/#/bulario/')
-driver.implicitly_wait(10)
+def get_bula(registerNum):
+    """A partir de um numero de registro (e empresa, opcional) abre o pdf respectivo
+    a bula do paciente daquele medicamento (banco de dados da ANVISA)"""
 
-# elements = writablle page section
+    options = Options()
+    options.set_preference("browser.download.folderList", 2)
+    options.set_preference("browser.download.manager.showWhenStarting", False)
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+    options.set_preference("pdfjs.disabled", False)  
+    options.set_preference("pdfjs.firstRun", True)
 
-# Numero de registro
-element = driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/form/div/div[2]/div[1]/div[2]/input")
-element.send_keys("183260244")
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    driver.get('https://consultas.anvisa.gov.br/#/bulario/')
+    driver.implicitly_wait(10)
 
-element = driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/form/div/div[3]/input[1]")
-element.click()
+    # elements = writablle page section
 
-element = driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/form/div/div[2]/table/tbody/tr[2]/td[6]/a")
-actions = ActionChains(driver)
-actions.context_click(element).perform()
+    # Numero de registro
+    element = driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/form/div/div[2]/div[1]/div[2]/input")
+    element.send_keys(registerNum)
+
+    element = driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/form/div/div[3]/input[1]")
+    element.click()
+
+    # element = driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/form/div/div[2]/table/tbody/tr[2]/td[6]/a")
+    # actions = ActionChains(driver)
+    # actions.context_click(element).perform()
+
+    botao_bula_paciente = driver.find_element(By.XPATH, "//a[@ng-if='produto.idBulaPacienteProtegido']")
+    botao_bula_paciente.click()
+
+# get_bula('183260244')
