53446c5cc89a1df5ef4a040e1f15cb535b62a10a
Matheus <m258602@dac.unicamp.br>
2026-02-12 15:52:59 +0000

    Json dump format fix

--- lembramed/api/src/anvisa_data/scraper.py
+++ lembramed/api/src/anvisa_data/scraper.py
@@ -8,28 +8,39 @@ def write_json(target_file, data):
 def get_pdf(pdf_path):
     """Returns a dictionary of topics extracted from each medication bula"""
     
-    # text = "\n".join(
-    #     p.get_textpage().get_text_range() 
-    #     for p in pdfium.PdfDocument(f"{pdf_path}")
-    # )
-    # print(text)
-    
     doc = fitz.open(pdf_path)
     full_text = ""
 
     for page in doc:
         full_text += page.get_text() + "\n"
 
-    padrao_titulos = r"(\n(?:\d+\.|DIZERES|ANEXO)\s+[A-ZÀ-Ú\s\?\/]+(?=\n))"
+    # padrao_titulos = r"(\n(?:\d+\.|DIZERES|ANEXO)\s+[A-ZÀ-Ú\s\?\/]+(?=\n))"
+    padrao_titulos = r"\n(\d+\.\s+[A-ZÀ-Ú0-9 ,\?\-]+)"
+
+    if "DIZERES LEGAIS" in full_text:
+        full_text = full_text.split("DIZERES LEGAIS")[0] + "DIZERES LEGAIS"
+
     partes = re.split(padrao_titulos, full_text)
-    dados_estruturados = {}
+    dados_estruturados = []
 
     if len(partes) > 1:
         for i in range(1, len(partes), 2):
-            titulo = partes[i].strip()
+            new_topic = {}
+            titulo = partes[i]
+            new_titulo = ""
+            for t in titulo:
+                if t.isalpha() or t == " ":
+                    new_titulo+=t
+            new_titulo = new_titulo.strip()
             conteudo = partes[i+1].strip() if i + 1 < len(partes) else ""
-            dados_estruturados[titulo] = conteudo
+            new_topic['titulo'] = new_titulo
+            new_topic['conteudo'] = conteudo
+            dados_estruturados.append(new_topic)
+    
+    dados_estruturados = [ d for d in dados_estruturados 
+                        if d["conteudo"].strip() != ""]
+
     return dados_estruturados
 
-resultado = get_pdf('data/bula_1770753006028.pdf')
-write_json("test", resultado)
+# resultado = get_pdf('data/bula_1770753006028.pdf')
+# write_json("test5", resultado)
