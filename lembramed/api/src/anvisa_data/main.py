82b4f45aee06c59ec68fb76981f544525cc34322
Matheus <m258602@dac.unicamp.br>
2026-03-06 16:53:15 +0000

    Changes to main logic

--- lembramed/api/src/anvisa_data/main.py
+++ lembramed/api/src/anvisa_data/main.py
@@ -1,6 +1,7 @@
 from web_navigator import download_bula, get_last_downloaded_file
 from scraper import get_pdf, write_json
 from excel_treatment import get_dict
+import os
 
 # abrir o json "anvisa_data.json" em 'data' e pegar o 
 # [4] de cada lista === registerNum
@@ -10,26 +11,15 @@ for key,value in anvisa_dados.items():
         registerNums = [f'{int(dados[4])}' for dados in 
         value if dados[4]]
 
-# output json do tipo:
-# {RegisterNum1: 
-#     [
-#         {   title: xxx 
-#             conteudo: xxx
-#         },
-#     ]
-# }
 data = {}
 
 # Pega a bula do paciente de todos os medicamentos registrados no excel da anvisa
 for num in registerNums:
-    if download_bula(num) == 0:
+    if download_bula(num) == 1:
         continue
     file_name = get_last_downloaded_file("data/")
-    data[num] = get_pdf(f"data/{file_name}")
+    if file_name != None:
+        data[num] = get_pdf(f"data/{file_name}")
+        os.remove(f"data/{file_name}")
 
-# Salva os dados 
 write_json("Bulario_data", data)
-
-# for num in registerNums:
-#     print(num)
-# print(f'Penultimo elemento: {registerNums[-2]},\nUltimo elemento: {registerNums[-1]}')
