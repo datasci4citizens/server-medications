7cee81ae2c54a9a149dd5ec825754575dbc6bfa9
Yanneti2 <m258602@dac.unicamp.br>
2026-03-13 02:05:55 +0000

    Automatic Leaflets Dataset implementation

--- lembramed/medication/apps.py
+++ lembramed/medication/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+from django.core.management import call_command
 
 class MedicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'medication'
+    def ready(self):
+        call_command("load_bula_data")
