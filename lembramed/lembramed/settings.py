6f0c0ec66af70d17414de64bfdea98e9d4d90914
Yanneti2 <m258602@dac.unicamp.br>
2026-03-29 14:49:39 +0000

    Google Auth feature add Closes #25

--- lembramed/lembramed/settings.py
+++ lembramed/lembramed/settings.py
@@ -163,6 +163,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 # If you want to allow credentials (cookies, etc.)
