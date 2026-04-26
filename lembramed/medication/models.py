4a32c76f771bf53516d1265a00e398a761c62bdc
MLeticiagomes <m281318@dac.unicamp.br>
2026-04-26 14:21:06 -0300

    medication model tests

--- lembramed/medication/models.py
+++ lembramed/medication/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from authentication.models import Person
-import uuid, datetime
-from datetime import datetime, timedelta, combine, date
+import uuid
+import datetime
+from datetime import datetime, timedelta, date 
 from django.utils import timezone
 from django.core.exceptions import ValidationError
 
-# Medication DATA from scrapers
 class Medication(models.Model):
     # RegisterNum for ANVISA, RxCUI for RxNorm:
     medication_id = models.IntegerField(primary_key = True)
@@ -46,7 +46,7 @@ class Take(models.Model):
 
     # comprimido/dosagem, OK
     # forma de medicacao (pilula...), ok
-    # horario, OK
+    # horario, OKa
     # quantidade, ok
     # lembrete de repor estoque (lembrete) ok
     # Int de prioridade da notificacao daquele medicamento
@@ -98,11 +98,11 @@ class TakeRecord(models.Model):
     ]
 
     cycle_type = models.CharField(max_length=10, choices=CYCLE_TYPE )
-    begin = models.DateField(default=datetime.date.today) 
-    end = models.DateField(default=datetime.date.today)
+    begin = models.DateField(default=datetime.today) 
+    end = models.DateField(default=datetime.today)
     days= models.CharField(max_length=50, null=True)
     take_at = models.TimeField(null=True, blank=True)  #first time you will take the medicine
-    take_cicle = models.IntegerField(null=True, blank=True) # ex: take in 8-8 hours...
+    take_cycle = models.IntegerField(null=True, blank=True) # ex: take in 8-8 hours...
     state = models.CharField(max_length=50, null=True) # taken, forgortten, late...
 
     def get_days_display(self):
@@ -116,7 +116,7 @@ class TakeRecord(models.Model):
         if self.cycle_type == 'daily' and not self.take_at:
             raise ValidationError("Informe o horário em que o medicamento será tomado")
 
-        if self.cycle_type == 'interval' and not self.take_cicle:
+        if self.cycle_type == 'interval' and not self.take_cycle:
              raise ValidationError("Informe de quanto em quanto tempo o medicamento será tomado")
         
         if self.cycle_type == 'interval' and not self.take_at:
@@ -132,17 +132,17 @@ class TakeRecord(models.Model):
         if self.cycle_type == 'daily' :
             return [self.take_at]
         
-        if self.cycle_type == 'interval' and self.take_cicle:
+        if self.cycle_type == 'interval' and self.take_cycle:
             scheduels = []
 
-            base_date = datetime.date.today()
-            current_dt = datetime.datetime.combine(base_date, self.take_at)
+            base_date = date.today()
+            current_dt = datetime.combine(base_date, self.take_at)
 
             total_hours = 0
             while total_hours < 24:
                 scheduels.append(current_dt.time())
-                current_dt += datetime.timedelta(hours=self.take_cicle)
-                tatal_hours += self.take_cicle
+                current_dt += timedelta(hours=self.take_cycle)
+                total_hours += self.take_cycle
 
                 if current_dt.date() > base_date:
                     break
@@ -151,77 +151,76 @@ class TakeRecord(models.Model):
         return []
     
 
-def define_state(self): # define the medication state based on when the medication was taken
+    def define_state(self): # define the medication state based on when the medication was taken
     
-    now = timezone.now()
-    current_now = timezone.localtime(now)
-    current_time = current_now.time()
-    current_date = current_now.date()
+        now = timezone.now() # review the when_was_taken 
+        current_now = timezone.localtime(now)
+        current_time = current_now.time()
+        current_date = current_now.date()
 
-    schedules = self.calculate_schedule()
-    if not schedules:
-        return None
-
-    # Find the closest time of the scheduel to now
-    past_schedules = [t for t in schedules if t <= current_time]
-    
-    if not past_schedules: 
-        return "waiting"
-
-    closest_scheduled_time = max(past_schedules) 
-
-    scheduled_datetime = combine(current_date, closest_scheduled_time) # convert to datetime
-    scheduled_datetime = timezone.make_aware(scheduled_datetime)
-
-    # time diference
-    time_gap = now - scheduled_datetime
-     
-    # future --> change the time limmit and atribute diferent time limits based on the medications priority 
-    if time_gap > timedelta(minutes=30):
-        self.state = "forgotten"
-    
-    if time_gap > timedelta(minutes=10) and  time_gap < timedelta(minutes=30):
-        self.state = "late"
-    else:
-        self.state = "taken" 
-
-    self.save() 
-    return self.state
-
-    
-def calculate_stock(self): # determine the amount of pills left
-        # future implementation --> other types of medications
-        if not self.taken_id.formato or self.taken_id.formato.lower() != 'pílula':
+        schedules = self.calculate_schedule()
+        if not schedules:
             return None
+
+        # Find the closest time of the scheduel to now
+        past_schedules = [t for t in schedules if t <= current_time]
         
-        try:
-            total_quantity = int(self.taken_id.quantity)
-        except (ValueError, TypeError):
-            return "Erro: Quantidade total de medicamentos não é um número válido."
+        if not past_schedules: 
+            return "waiting"
 
-        days_passed = (date.today() - self.begin).days
+        closest_scheduled_time = max(past_schedules) 
 
-        days_passed = max(0, days_passed) # if is the first day
+        scheduled_datetime = datetime.combine(current_date, closest_scheduled_time) # convert to datetime
+        scheduled_datetime = timezone.make_aware(scheduled_datetime)
 
-        if self.cycle_type == 'daily': 
-            medications_taken = days_passed
-            week_medication = 7
-                
-        elif self.cycle_type == 'interval' and self.take_cycle:
-            medications_per_day = 24 // self.take_cycle
-            medications_taken = days_passed * medications_per_day
-            week_medication = 7*medications_per_day
+        # time diference
+        time_gap = now - scheduled_datetime
         
-        medication_left = total_quantity - medications_taken
-        time_left = (self.end - date.today()).days
+        # future --> change the time limmit and atribute diferent time limits based on the medications priority 
+        if time_gap > timedelta(minutes=30):
+            self.state = "forgotten"
+        elif time_gap > timedelta(minutes=10):
+            self.state = "late"
+        else:
+            self.state = "taken"
+
+        self.save() 
+        return self.state
+
+        
+    def calculate_stock(self): # determine the amount of pills left
+            # future implementation --> other types of medications
+            if not self.taken_id.formato or self.taken_id.formato.lower() != 'pílula':
+                return None
+            
+            try:
+                total_quantity = int(self.taken_id.quantity)
+            except (ValueError, TypeError):
+                return "Erro: Quantidade total de medicamentos não é um número válido."
+
+            days_passed = (date.today() - self.begin).days
+
+            days_passed = max(0, days_passed) # if is the first day
+
+            if self.cycle_type == 'daily': 
+                medications_taken = days_passed
+                week_medication = 7
+                    
+            elif self.cycle_type == 'interval' and self.take_cycle:
+                medications_per_day = 24 // self.take_cycle
+                medications_taken = days_passed * medications_per_day
+                week_medication = 7*medications_per_day
+            
+            medication_left = total_quantity - medications_taken
+            time_left = (self.end - date.today()).days
 
 
-        if medication_left <= week_medication and time_left> 7: 
-            # if the amount of pills left are less than the amount required in a week 
-            # the amount of time left to take the medication is more than a week
-            return f"Você tem {medication_left} pílulas restantes, reponha seu estoque."
-          
+            if medication_left <= week_medication and time_left> 7: 
+                # if the amount of pills left are less than the amount required in a week 
+                # the amount of time left to take the medication is more than a week
+                return f"Você tem {medication_left} pílulas restantes, reponha seu estoque."
+            
 
-def __str__(self):
-        horarios = ", ".join([t.strftime('%H:%M') for t in self.calculate_schedule()])
-        return f"{self.taken_id} - Horários: {horarios}"
+    def __str__(self):
+            horarios = ", ".join([t.strftime('%H:%M') for t in self.calculate_schedule()])
+            return f"{self.taken_id} - Horários: {horarios}"
